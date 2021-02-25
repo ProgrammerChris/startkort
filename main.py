f749994da40ea9d92be6ad168dea4737d20043e3
Chris <45407957+ProgrammerChris@users.noreply.github.com>
2021-02-25 13:33:19 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -11,26 +11,21 @@ def auto_kort(antall):
 
     url = 'http://start.deepwater.com/'
 
-    os.remove("dagens.txt")
+    os.remove("brukte.txt")
 
-    personer_fil = open("personer.txt", "r", encoding='utf-8')
     kort_fil = open("kort.txt", "r", encoding='utf-8')
     
     kort_liste = []
-    personer = []
     tid = 0
+
+    print("Startkort presse starter!")
 
     for kort in kort_fil:
         kort_liste.append(kort)
 
-    for person in personer_fil:
-        personer.append(person)
-
-    random.shuffle(personer)
     random.shuffle(kort_liste)
 
     for x in range(antall):
-        person = random.choice(personer)
         kort_text = random.choice(kort_liste)
         kort_liste.remove(kort_text)
 
@@ -57,10 +52,10 @@ def auto_kort(antall):
         browser['field_unit_transocean[und]'] = 'END - Transocean Endurance'
 
         #Name
-        browser['field_name[und][0][value]'] = '.'
+        browser['field_name[und][0][value]'] = '*'
 
         #First Name
-        browser['field_first_name[und][0][value]'] = '.'
+        browser['field_first_name[und][0][value]'] = '*'
 
         #The agreement
         browser['field_agreement_transocean[und]'] = '0'
@@ -85,19 +80,24 @@ def auto_kort(antall):
         if response.status_code == 200:
           
           # Skriver kort til fil så man kan se hva som er sendt.
-          sendte_kort = open("dagens.txt", "a")
+          sendte_kort = open("brukte.txt", "a")
           sendte_kort.write(kort_text)
           sendte_kort.close()
 
           print('Startkort ' + str(x+1) + ' av ' + str(antall) + ' sendt!')
           
           if (x+1) != antall:
-            tid = random.randint(60, 120) # 60-600 sekunder pause mellom hvert kort
+            tid = random.randint(60, 600) # 60-600 sekunder pause mellom hvert kort
             print('Neste kort sendes om ' + "%.2f" % (tid/60) + ' minutter')
             time.sleep(tid)
 
-    personer_fil.close()
+    ubrukte_kort = open("ubrukte.txt", "a")
+    for kort in kort_liste:
+      ubrukte_kort.write(kort)
+
+    ubrukte_kort.close()
     kort_fil.close()
+    print("Startkort presse stoppet!")
 
 if __name__ == '__main__':
-    auto_kort(10)
+    auto_kort(1)
